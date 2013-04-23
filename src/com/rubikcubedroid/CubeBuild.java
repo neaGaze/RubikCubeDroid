660e1779d60495a6f53cc39b72bc8f8ed9ddecfb
neaGaze <nizeshshakya@gmail.com>
2013-04-23 22:48:41 +0545

    2nd Commit with Cube and Color

--- src/com/rubikcubedroid/CubeBuild.java
+++ src/com/rubikcubedroid/CubeBuild.java
@@ -12,42 +12,10 @@ public class CubeBuild {
 	
 	private ByteBuffer byteBuffer;
 	private FloatBuffer vertexBuffer;
-	/** The buffer holding the normals */
-	private FloatBuffer normalBuffer;
 	private FloatBuffer colorBuffer;
 	
 	private float[] vertices= {
-		/*	// Vertices according to faces
-			-1.0f, -1.0f, 1.0f, //v0
-			1.0f, -1.0f, 1.0f, 	//v1
-			-1.0f, 1.0f, 1.0f, 	//v2
-			1.0f, 1.0f, 1.0f, 	//v3
-
-			1.0f, -1.0f, 1.0f, 	//...
-			1.0f, -1.0f, -1.0f, 
-			1.0f, 1.0f, 1.0f, 
-			1.0f, 1.0f, -1.0f,
-
-			1.0f, -1.0f, -1.0f, 
-			-1.0f, -1.0f, -1.0f, 
-			1.0f, 1.0f, -1.0f, 
-			-1.0f, 1.0f, -1.0f,
-
-			-1.0f, -1.0f, -1.0f, 
-			-1.0f, -1.0f, 1.0f, 
-			-1.0f, 1.0f, -1.0f, 
-			-1.0f, 1.0f, 1.0f,
-
-			-1.0f, -1.0f, -1.0f, 
-			1.0f, -1.0f, -1.0f, 
-			-1.0f, -1.0f, 1.0f, 
-			1.0f, -1.0f, 1.0f,
-
-			-1.0f, 1.0f, 1.0f, 
-			1.0f, 1.0f, 1.0f, 
-			-1.0f, 1.0f, -1.0f, 
-			1.0f, 1.0f, -1.0f, 
-			*/
+			
 			 -1.0f, -1.0f, -1.0f,	//lower back left (0)
 	            1.0f, -1.0f, -1.0f,		//lower back right (1)
 	            1.0f,  1.0f, -1.0f,		//upper back right (2)
@@ -55,21 +23,12 @@ public class CubeBuild {
 	            -1.0f, -1.0f,  1.0f,	//lower front left (4)
 	            1.0f, -1.0f,  1.0f,		//lower front right (5)
 	            1.0f,  1.0f,  1.0f,		//upper front right (6)
-	            -1.0f,  1.0f,  1.0f		//upper front left (7)
+	            -1.0f,  1.0f,  1.0f		//upper front left (7)   
 			
 	};
 	
 	private byte indices[] = {
-			
-			// Faces definition
-		/*	0, 1, 3, 0, 3, 2, 		// Face front
-			4, 5, 7, 4, 7, 6, 		// Face right
-			8, 9, 11, 8, 11, 10, 	// ...
-			12, 13, 15, 12, 15, 14, 
-			16, 17, 19, 16, 19, 18, 
-			20, 21, 23, 20, 23, 22, 
 			0, 4, 5,    0, 5, 1,
-            //and so on...  */
             1, 5, 6,    1, 6, 2,
             2, 6, 7,    2, 7, 3,
             3, 7, 4,    3, 4, 0,
@@ -77,49 +36,17 @@ public class CubeBuild {
             3, 0, 1,    3, 1, 2    
 	};
 	
-	private float normals[] = {
-			//Normals
-			0.0f, 0.0f, 1.0f, 						
-			0.0f, 0.0f, -1.0f, 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, -1.0f, 0.0f, 
-			
-			0.0f, 0.0f, 1.0f, 
-			0.0f, 0.0f, -1.0f, 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, -1.0f, 0.0f,
-			
-			0.0f, 0.0f, 1.0f, 
-			0.0f, 0.0f, -1.0f, 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, -1.0f, 0.0f,
-			
-			0.0f, 0.0f, 1.0f, 
-			0.0f, 0.0f, -1.0f, 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, -1.0f, 0.0f,
-			
-			0.0f, 0.0f, 1.0f, 
-			0.0f, 0.0f, -1.0f, 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, -1.0f, 0.0f,
-			
-			0.0f, 0.0f, 1.0f, 
-			0.0f, 0.0f, -1.0f, 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, -1.0f, 0.0f,
-								};
 	
 	 /** The initial color definition */	
 		private float colors[] = {
-				            0.0f,  1.0f,  0.0f,  1.0f,
-				            0.0f,  1.0f,  0.0f,  1.0f,
-				            1.0f,  0.5f,  0.0f,  1.0f,
-				            1.0f,  0.5f,  0.0f,  1.0f,
-				            1.0f,  0.0f,  0.0f,  1.0f,
-				            1.0f,  0.0f,  0.0f,  1.0f,
+				            0.0f,  1.0f,  0.0f,  1.0f,  //Position 0 : compatible with Position 4 
+				            0.0f,  0.0f,  1.0f,  1.0f,	
 				            0.0f,  0.0f,  1.0f,  1.0f,
-				            1.0f,  0.0f,  1.0f,  1.0f
+				            1.0f,  0.0f,  0.0f,  1.0f,  //Position 3 : compatible with Position 7 
+				            0.0f,  1.0f,  0.0f,  1.0f,	//Position 4 : compatible with Position 0
+				            1.0f,  1.0f,  0.0f,  1.0f,
+				            1.0f,  1.0f,  0.0f,  1.0f,
+				            1.0f,  0.0f,  0.0f,  1.0f	//Position 7 : compatible with Position 3 
 				    								};
 	   
 	public CubeBuild()
@@ -134,11 +61,6 @@ public class CubeBuild {
 				byteBuffer.put(indices);  
 				byteBuffer.position(0); 
 				
-				tmpBuffer = ByteBuffer.allocateDirect(normals.length * 4);
-				tmpBuffer.order(ByteOrder.nativeOrder());
-				normalBuffer = tmpBuffer.asFloatBuffer();
-				normalBuffer.put(normals);
-				normalBuffer.position(0);
 				//
 				tmpBuffer = ByteBuffer.allocateDirect(colors.length * 4);
 				tmpBuffer.order(ByteOrder.nativeOrder());
@@ -151,16 +73,15 @@ public class CubeBuild {
 	public void draw(GL10 gl10)
 	{	
 		gl10.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-		gl10.glEnableClientState(GL10.GL_NORMAL_ARRAY);
 		gl10.glEnableClientState(GL10.GL_COLOR_ARRAY);
 		
 		gl10.glFrontFace(GL10.GL_CCW);		//set the face rotation
 		gl10.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
-		gl10.glNormalPointer(GL10.GL_FLOAT, 0, normalBuffer);
+		gl10.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
 		
 		gl10.glDrawElements(GL10.GL_TRIANGLES, indices.length, GL10.GL_UNSIGNED_BYTE, byteBuffer);
+		
 		gl10.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-		gl10.glDisableClientState(GL10.GL_NORMAL_ARRAY);
 		gl10.glDisableClientState(GL10.GL_COLOR_ARRAY);
 		//Log.v("ERROR ON CUBEBUILD","draw Error");
 	}
--- src/com/rubikcubedroid/RubikCube.java
+++ src/com/rubikcubedroid/RubikCube.java
@@ -23,8 +23,9 @@ public class RubikCube extends GLSurfaceView implements GLSurfaceView.Renderer{
 		// Log.v("ERROR ON RUBIKCUBE","Cube.draw()");
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 		gl.glLoadIdentity();
-		gl.glTranslatef(0.0f, 0.0f, -2.0f);
+		gl.glTranslatef(0.0f, 0.0f, -7.0f);
 		gl.glScalef(0.8f, 0.8f, 0.8f);  
+		gl.glRotatef(50.0f, 20.0f, 30.0f, 40.0f);
 		cube.draw(gl);
 	}
 
@@ -43,7 +44,7 @@ public class RubikCube extends GLSurfaceView implements GLSurfaceView.Renderer{
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		// TODO Auto-generated method stub
 	//	Log.v("ERROR ON SurfaceChanged", "error cha huna sakcha");
-		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glShadeModel(GL10.GL_FLAT);
 		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
 		gl.glClearDepthf(1.0f);
 		gl.glEnable(GL10.GL_DEPTH_TEST);

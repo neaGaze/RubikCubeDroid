56aaabaab18fea51e857ba0650e3f1bd29163810
neaGaze <nizeshshakya@gmail.com>
2013-05-06 14:19:57 +0545

    Third Commit with group rotation prototype  complete :D

--- src/com/rubikcubedroid/RubikCube.java
+++ src/com/rubikcubedroid/RubikCube.java
@@ -1,5 +1,6 @@
 package com.rubikcubedroid;
 
+import com.rubikcubedroid.*;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -7,10 +8,12 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
 import android.util.Log;
+import android.widget.Toast;
 
 public class RubikCube extends GLSurfaceView implements GLSurfaceView.Renderer{
 
 	private CubeBuild[] cube = new CubeBuild[27];
+	float rot = 0.0f, cubeRot = 0.0f;
 	public RubikCube(Context context) {
 		super(context);
 		for(int i =0; i<27; i++)
@@ -27,29 +30,45 @@ public class RubikCube extends GLSurfaceView implements GLSurfaceView.Renderer{
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 		gl.glLoadIdentity();
 		gl.glScalef(0.8f, 0.8f, 0.8f);  
-		
+	/*	
+	 * 
+	 * This is jsut to work with the Colors
+	 * 
 		gl.glLoadIdentity();
 		gl.glTranslatef(0.0f, 0.0f, -7.0f);
 		gl.glRotatef(30.0f, 30.0f, 30.0f, 30.0f);
 	//	gl.glRotatef(210.0f, 210.0f, 210.0f, 150.0f);
-		cube[0].draw(gl);
-	/*	
+	//	cube[0].draw(gl);
+		*/
 		int k=0;
 
+		gl.glPushMatrix();
+		gl.glRotatef(cubeRot, 0.0f, 0.0f ,1.0f);
 		for(int l=0; l<3; l++)
 		{
-			for(int i=0; i<3; i++)
+			if(l == 2)  //To rotate only the first front polygon in 1.7f angle
 			{
+			gl.glPushMatrix();
+			gl.glRotatef(rot, 0.0f, 0.0f, 1.0f);
+			}
+			for(int i=0; i<3; i++)
+			{				
 				for(int j=0; j<3; j++)
 				{
-					gl.glLoadIdentity();
+					gl.glPushMatrix();
 					gl.glTranslatef(-2.1f+(2.1f*i), -2.1f+(2.1f*j), -23.1f+(2.1f*l));
-	//				gl.glRotatef(50.0f, 20.0f, 30.0f, 40.0f);
-					gl.glRotatef(200.0f, 200.0f, 30.0f, 40.0f);
 					cube[k++].draw(gl);
+					gl.glPopMatrix();
 				}
+				
 			}
-		}   */
+			if(l ==2)
+			{
+			gl.glPopMatrix();
+			rot += 1.7f;  }
+		}
+		gl.glPopMatrix();
+		cubeRot -= 0.5f;
 	}
 
 	@Override
